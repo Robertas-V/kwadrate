52470104415a4c56b959f621c33277b31d72bcac
Evaldas Buinauskas <buinauskas.evaldas@gmail.com>
2018-12-28 18:53:52 +0200

    Added dummy toolbar Added shared module to contain reusables

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,40 @@
-import { TestBed, async } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+    let component: AppComponent;
+    let fixture: ComponentFixture<AppComponent>;
+
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             imports: [RouterTestingModule],
-            declarations: [AppComponent]
+            declarations: [AppComponent],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA]
         }).compileComponents();
     }));
 
-    it('should create the app', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
-        expect(app).toBeTruthy();
-    });
-
-    it(`should have as title 'kwadrate'`, () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
-        expect(app.title).toEqual('kwadrate');
-    });
-
-    it('should render title in a h1 tag', () => {
-        const fixture = TestBed.createComponent(AppComponent);
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        component = fixture.componentInstance;
         fixture.detectChanges();
-        const compiled = fixture.debugElement.nativeElement;
-        expect(compiled.querySelector('h1').textContent).toContain('Welcome to kwadrate!');
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should contain app toolbar', () => {
+        const appToolbar = fixture.debugElement.query(By.css('app-toolbar'));
+
+        expect(appToolbar).toBeDefined();
+    });
+
+    it('should contain router outlet', () => {
+        const routerOutlet = fixture.debugElement.query(By.css('router-outlet'));
+
+        expect(routerOutlet).toBeDefined();
     });
 });
