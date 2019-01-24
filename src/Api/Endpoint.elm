c841b7bec220a29a05db6a83deadc95483d9321e
Evaldas <7301441+buinauskas@users.noreply.github.com>
2019-01-24 11:59:57 +0200

    Added categories together with a dummy json DB (#13)
    * Sample code that renders a list of hardcoded todos
    
    * Decoding usinng NoRedInk's solution, it's far more elegant than built in one
    
    * A trivial code where multiple component can communicate
    
    * Getting todos from a restful API works!
    
    * Also have added categories as an sample for
    
    * Added a break point

---
+++ src/Api/Endpoint.elm
@@ -0,0 +1,65 @@
+module Api.Endpoint exposing (Endpoint, categories)
+
+import Http exposing (Body, Expect, Header, request)
+import Url.Builder exposing (QueryParameter)
+
+
+{-| Http.request, except it takes an Endpoint instead of a Url.
+-}
+request :
+    { method : String
+    , headers : List Header
+    , url : String
+    , body : Body
+    , expect : Expect msg
+    , timeout : Maybe Float
+    , tracker : Maybe String
+    }
+    -> Cmd msg
+request config =
+    Http.request
+        { method = config.method
+        , headers = config.headers
+        , url = config.url
+        , body = config.body
+        , expect = config.expect
+        , timeout = config.timeout
+        , tracker = config.tracker
+        }
+
+
+
+-- TYPES
+
+
+{-| Get a URL to the Conduit API.
+
+This is not publicly exposed, because we want to make sure the only way to get one of these URLs is from this module.
+
+-}
+type Endpoint
+    = Endpoint String
+
+
+unwrap : Endpoint -> String
+unwrap (Endpoint str) =
+    str
+
+
+url : List String -> List QueryParameter -> Endpoint
+url paths queryParams =
+    -- NOTE: Url.Builder takes care of percent-encoding special URL characters.
+    -- See https://package.elm-lang.org/packages/elm/url/latest/Url#percentEncode
+    Url.Builder.crossOrigin "http://localhost:3000"
+        ("api" :: paths)
+        queryParams
+        |> Endpoint
+
+
+
+-- ENDPOINTS
+
+
+categories : Endpoint
+categories =
+    url [ "categories" ] []
---
+++ src/Page/Categories.elm
@@ -0,0 +1,103 @@
+module Page.Categories exposing (Model, Msg(..), init, subscriptions, update, view)
+
+import Api.Endpoint as Endpoint
+import Category exposing (Category, categoryListDecoder)
+import Debug
+import Html exposing (Html, br, button, div, h1, li, pre, text, ul)
+import Html.Events exposing (onClick)
+import Http
+import Json.Decode as Decode
+
+
+
+-- TYPES
+
+
+type Model
+    = NoOp
+    | Loading
+    | Failure String
+    | Success (List Category)
+
+
+type Msg
+    = GetCategories
+    | CategoriesResult (Result Http.Error (List Category))
+
+
+
+-- MODEL
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( NoOp, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    let
+        toLi : String -> Html Msg
+        toLi s =
+            li [] [ text s ]
+    in
+    div []
+        [ button [ onClick GetCategories ] [ text "Get Categories" ]
+        , br [] []
+        , case model of
+            NoOp ->
+                text ""
+
+            Loading ->
+                text "Loading"
+
+            Failure reason ->
+                text reason
+
+            Success categories ->
+                ul [] (categories |> List.map .name |> List.map toLi)
+        ]
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update message model =
+    case message of
+        GetCategories ->
+            ( Loading, getCategories )
+
+        CategoriesResult res ->
+            case res of
+                Err _ ->
+                    ( Failure "Crap happened bruh", Cmd.none )
+
+                Ok categories ->
+                    ( Success categories, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- HTTP
+
+
+getCategories : Cmd Msg
+getCategories =
+    Http.get
+        { url = "http://localhost:3000/categories"
+        , expect = Http.expectJson CategoriesResult categoryListDecoder
+        }
